3494b422be7ce1b780b29a60208f1f7610674805
Sara Pettigrew <sarajp.ak@hotmail.com>
2021-05-13 22:46:46 -0600

    Set up comp, but using this.props. Bleh!

---
+++ contacts/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class ContactList extends Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
+class App extends Component {
+  render() {
+  return (
+    <div className="App">
+      <ContactList contacts={[
+          { name: 'Tyler '},
+          { name: 'Karen' },
+          { name: 'Richard '}
+        ]}/>
+        <ContactList contacts={[
+          { name: 'Amanda' },
+          { name: 'Mikenzie' },
+          { name: 'Ryan' }
+        ]}/>
+
+    </div>
+  );
+}
+}
+
+export default App;
