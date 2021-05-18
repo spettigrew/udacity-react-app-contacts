97142e1a27ba13354f7bc45630c598c8f28a46b2
Sara Pettigrew <sarajp.ak@hotmail.com>
2021-05-17 21:25:27 -0600

    Contacts component set up with array of names

---
+++ contacts/src/Contacts.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const contacts = [
+    {
+      "id": "karen",
+      "name": "Karen Isgrigg",
+      "handle": "karen_isgrigg",
+      "avatarURL": "http://localhost:5001/karen.jpg"
+    },
+    {
+      "id": "richard",
+      "name": "Richard Kalehoff",
+      "handle": "richardkalehoff",
+      "avatarURL": "http://localhost:5001/richard.jpg"
+    },
+    {
+      "id": "tyler",
+      "name": "Tyler McGinnis",
+      "handle": "tylermcginnis",
+      "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+   ];
+   
